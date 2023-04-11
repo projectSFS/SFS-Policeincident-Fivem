f1e5e70e8da3b7defdd2e05ba63ecc82e9376e92
SFS Projects <130466738+projectSFS@users.noreply.github.com>
2023-04-12 02:13:57 +0530

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+-- For support join my discord: https://discord.gg/zzUfkfRHzP
+author "sfs"
+description "nothing"
+version "1.0"
+fx_version "cerulean"
+game "gta5"
+lua54 'yes'
+shared_script {
+    '@ox_lib/init.lua',
+"sv_config.lua"} 
+
+client_scripts {
+    "client.lua"
+}
+server_scripts {
+    
+    "server.lua"
+}
+
+escrow_ignore {
+    "sv_config.lua"
+}
+dependency "ox_lib"
+dependency '/assetpacks'
