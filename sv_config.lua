cf58ff90a1c90684fdd321a3410ba277d0648268
SFS Projects <130466738+projectSFS@users.noreply.github.com>
2023-04-12 02:21:30 +0530

    Update sv_config.lua

--- sv_config.lua
+++ sv_config.lua
@@ -1,5 +1,3 @@
-Config = {
-    DiscordWebhook = 'https://discord.com/api/webhooks/1081494848420462632/NHYnSwh6uHOKFz1XG4Qy5QXrXvengKY49kFZnXJlzXthQF7cZf5uoaWxuK-Q72UBEbvq', -- Do not forget to add a webhook.
-    appicon = 'https://cdn.discordapp.com/attachments/1082682837150478416/1087998459124121670/128x128.png', -- must be a 128x128 pixel image [url type = discordapp attachment image]
-    Botname = 'POLICE INCIDENT',
-}
+SEE README FILE 
+CLICK THE TEBEX LINK
+download this file on tebex its free 
